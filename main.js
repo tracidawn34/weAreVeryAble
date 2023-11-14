324c4e9337ba757767b916b32849fc34973fef22
unknown <tmcfall.prsvr@gmail.com>
2023-11-14 11:26:41 -0500

    final

---
+++ main.js
@@ -0,0 +1,80 @@
+/******************
+ * YOUR CODE BELOW *
+ ******************/
+let myFirstName ="Traci"
+let myLastName ="Mcfall"
+let myFavoriteCeleb ="Zack Bryan"
+let mySentence ="none"
+let myLuckyNum = 13
+let myAge = 31
+let myHeight = 62
+ let myFavoriteYear = 2009
+ let mySum = 5 + 2317 + 36
+ let myProduct = 232 * 43 / 2 + 4
+ let myHeightAndAge = 31 + 62
+
+/*********************************************************************************************************************
+
+ * CODE BELOW HERE IS FOR INTERNAL USE ONLY! IT ENSURES THAT THE TESTING *
+ * CAN READ AND EVALUATE YOUR CODE. *
+ * DON'T CHANGE ANYTHING BELOW HERE, PLEASE! *
+
+ *********************************************************************************************************************/
+
+if (typeof myFirstName === "undefined") {
+  myFirstName = undefined;
+}
+
+if (typeof myLastName === "undefined") {
+  myLastName = undefined;
+}
+
+if (typeof myFavoriteCeleb === "undefined") {
+  myFavoriteCeleb = undefined;
+}
+
+if (typeof mySentence === "undefined") {
+  mySentence = undefined;
+}
+
+if (typeof myLuckyNum === "undefined") {
+  myLuckyNum = undefined;
+}
+
+if (typeof myAge === "undefined") {
+  myAge = undefined;
+}
+
+if (typeof myHeight === "undefined") {
+  myHeight = undefined;
+}
+
+if (typeof myFavoriteYear === "undefined") {
+  myFavoriteYear = undefined;
+}
+
+if (typeof mySum === "undefined") {
+  mySum = undefined;
+}
+
+if (typeof myProduct === "undefined") {
+  myProduct = undefined;
+}
+
+if (typeof myHeightAndAge === "undefined") {
+  myHeightAndAge = undefined;
+}
+
+module.exports = {
+  myFirstName,
+  myLastName,
+  myFavoriteCeleb,
+  mySentence,
+  myLuckyNum,
+  myAge,
+  myHeight,
+  myFavoriteYear,
+  mySum,
+  myProduct,
+  myHeightAndAge,
+};
